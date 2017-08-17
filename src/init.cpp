085db5951f7020fc44110c1d9b3fea8012da1bc5
utabit <dev@utabit.com>
2017-08-17 17:20:13 +0430

    Update init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -481,8 +481,8 @@ std::string HelpMessage(HelpMessageMode mode)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/utabit/utabit13>";
-    const std::string URL_WEBSITE = "<https://utabitcore.org>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/utabit/utabit>";
+    const std::string URL_WEBSITE = "<https://utabit.com>";
     // todo: remove urls from translations on next change
     return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2017, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
